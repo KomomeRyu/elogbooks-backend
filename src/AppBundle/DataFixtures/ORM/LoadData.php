1fc402bd6f276ea47220ea7c28660086116cd46b
pepe barrientos <pepe@barrientos.soy>
2019-01-26 13:39:35 +0000

    user fixture load

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -5,6 +5,7 @@ namespace AppBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\Job;
+use AppBundle\Entity\User;
 
 /**
  * Class LoadJobData
@@ -31,6 +32,39 @@ class LoadJobData implements FixtureInterface
             $job->setDescription($jobDescription);
 
             $manager->persist($job);
+        }
+
+        $manager->flush();
+    }
+}
+
+class LoadUserData implements FixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $users = [
+           [
+               'name' => 'Pepe',
+               'email' => 'pepe@pepe.com',
+               'job_id' => '0'
+           ],
+           [
+            'name' => 'Juan',
+            'email' => 'juan@juan.com',
+            'job_id' => '0'
+        ],
+        ];
+
+        foreach ($users as $userDetails) {
+            $user = new User();
+            $user->setName($userDetails['name']);
+            $user->setEmail($userDetails['email']);
+            $user->setJobId($userDetails['job_id']);
+
+            $manager->persist($user);
         }
 
         $manager->flush();
